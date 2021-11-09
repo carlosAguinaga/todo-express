7354723b28b43b39ea2ac811ecdfb7f7b41d6f05
Carlos <carlosaguinaga01@gmail.com>
2021-11-09 11:47:40 -0500

    import path removed

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const tasksRoutes = require('./routes/tasks.routes');
 const middlewareError = require('./middlewares/error.middleware');
-const path = require('path');
 
 const app = express();
 
